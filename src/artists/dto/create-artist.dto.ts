9e79e37fd52ea4b9be52a315e3401d210b6781fc
foxnorth228 <khitrii03@gmail.com>
2022-07-13 13:02:22 +0300

    feat: implement artist.servise

---
+++ src/artists/dto/create-artist.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsBoolean, IsNotEmpty} from "class-validator";
+
+export class CreateArtistDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsBoolean()
+    @IsNotEmpty()
+    grammy: boolean;
+    
+}
--- src/artists/services/artist.service.ts
+++ src/artists/services/artist.service.ts
@@ -1,8 +1,55 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, BadRequestException, NotFoundException } from "@nestjs/common";
 import { IArtist } from "../interfaces/artist.interface";
+import { CreateArtistDto } from "../dto/create-artist.dto";
+import { validate, v4 } from "uuid"
+
+const checkValidation = (id) => { 
+    if(!validate(id)) {
+        throw new BadRequestException(`This id: "${id}" is not valid`);
+    } 
+};
 
 @Injectable()
 export class ArtistService {
-
     private readonly artists: IArtist[] = [];
+
+    findAll() {
+        return this.artists;
+    }
+
+    findOne(id: string) {
+        checkValidation(id);
+        const artist = this.artists.find((el) => el.id === id);
+        if(artist) {
+            return artist;
+        } else {
+            throw new NotFoundException(`Artist with id: "${id}" is not exist`);
+        }
+    }
+
+    create(createArtist: CreateArtistDto) {
+        const artist = Object.assign({ id: v4() }, createArtist);
+        this.artists.push(artist);
+
+    }
+
+    update(id: string, createArtist: CreateArtistDto) {
+        checkValidation(id);
+        const artist = this.artists.find((el) => el.id === id);
+        if(!artist) {
+            throw new NotFoundException(`Artist with id: "${id}" is not exist`);
+        }
+        artist.name = createArtist.name;
+        artist.grammy = createArtist.grammy;
+        return artist;
+    }
+
+    delete(id: string) {
+        checkValidation(id);
+        const artistIndex = this.artists.findIndex((el) => el.id === id);
+        if(artistIndex === -1) {
+            throw new NotFoundException(`Artist with id: "${id}" is not exist`);
+        }
+        this.artists.splice(artistIndex, 1);
+    }
 }
