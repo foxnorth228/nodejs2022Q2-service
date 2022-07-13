5cf96c985caac3f0e3d0e2405b165c23403fb881
foxnorth228 <khitrii03@gmail.com>
2022-07-13 12:34:48 +0300

    fead: create artists module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FavsController } from './controllers/favs.controller';
 import { UserController } from './users/user.controller';
-import { TrackController } from './controllers/track.controller';
-import { AlbumController } from './controllers/album.controller';
-import { ArtistController } from './controllers/artist.controller';
+import { ArtistController } from './artists/artist.controller';
 import { UserService } from "./users/services/user.service";
+import { ArtistService } from './artists/services/artist.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, AlbumController, 
-    ArtistController, TrackController, FavsController],
+  controllers: [AppController, UserController, ArtistController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
