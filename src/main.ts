89134120c8fbfeecfbaa50f315b975bf5af1a025
foxnorth228 <khitrii03@gmail.com>
2022-07-18 09:34:13 +0300

    fix: fix lint warnings

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,17 @@ import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { 
-  DocumentBuilder,
-  SwaggerModule
-} from "@nestjs/swagger";
-import { parse } from "yaml";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 import { readFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const file = readFileSync("doc/api.yaml");
+  const file = readFileSync('doc/api.yaml');
   const yamlBody = parse(file.toString('utf8'));
   const options = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, yamlBody);
-  SwaggerModule.setup("/api", app, document);
+  SwaggerModule.setup('/api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 }
