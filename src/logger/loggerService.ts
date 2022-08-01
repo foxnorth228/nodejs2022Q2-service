3f94742fa1344ad57dd8fb982341d4fa1a27aa8d
foxnorth228 <khitrii03@gmail.com>
2022-08-01 11:05:10 +0300

    feat: add FileLogger module

---
+++ src/logger/loggerService.ts
@@ -0,0 +1,13 @@
+import { LoggerService } from "@nestjs/common";
+
+export class FileLogger implements LoggerService {
+    log (message: any, ...optionalParams: any[]) {
+
+    }
+    warn (message: any, ...optionalParams: any[]) {
+        
+    }
+    error (message: any, ...optionalParams: any[]) {
+        
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { parse } from 'yaml';
 import { readFileSync } from 'fs';
+import { FileLogger } from "./logger/loggerService";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new FileLogger(),
+  });
   const file = readFileSync('doc/api.yaml');
   const yamlBody = parse(file.toString('utf8'));
   const document = SwaggerModule.createDocument(app, yamlBody);
